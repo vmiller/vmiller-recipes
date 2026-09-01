87be2d5e1d3b37b9368488b731fa441430b11b02
Vaughn Miller <vemiller@gmail.com>
2026-09-01 15:43:48 -0400

    Add GlobalProtect recipes (#82)
    Add GlobalProtect download and munki recipes
    
    Add new URLHighestVersionTextSearcher shared processor

---
+++ SharedProcessors/URLHighestVersionTextSearcher.py
@@ -0,0 +1,140 @@
+#!/usr/local/autopkg/python
+#
+# Vaughn Miller 2026
+#
+# Based on URLTextSearcher processor :
+#   Refactoring 2018 Michal Moravec
+#   Copyright 2015 Greg Neagle
+#   Based on URLTextSearcher.py, Copyright 2014 Jesse Peterson
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+"""See docstring for URLTextSearcher class"""
+
+import re
+from packaging.version import Version
+
+from autopkglib import ProcessorError
+from autopkglib.URLGetter import URLGetter
+
+MATCH_MESSAGE = "Found matching text"
+NO_MATCH_MESSAGE = "No match found on URL"
+
+__all__ = ["URLHighestVersionTextSearcher"]
+
+
+class URLHighestVersionTextSearcher(URLGetter):
+    """Downloads a URL using curl and performs a regular expression match
+    on the text to get a list of values.  Returns the highest value from the list."""
+
+    description = __doc__
+    lifecycle = {"introduced": "0.2.9"}
+    input_variables = {
+        "re_pattern": {
+            "required": True,
+            "description": "Regular expression (Python) to match against page.",
+        },
+        "url": {
+            "required": True,
+            "description": "URL to search for the regex pattern.",
+        },
+        "result_output_var_name": {
+            "required": False,
+            "description": (
+                "The name of the output variable that is returned "
+                "by the match. If not specified then a default of "
+                '"match" will be used.'
+            ),
+            "default": "match",
+        },
+        "request_headers": {
+            "required": False,
+            "description": (
+                "Optional dictionary of headers to include with "
+                "the download request."
+            ),
+        },
+        "curl_opts": {
+            "required": False,
+            "description": (
+                "Optional array of curl options to include with "
+                "the download request."
+            ),
+        },
+        "re_flags": {
+            "required": False,
+            "description": (
+                "Optional array of strings of Python regular "
+                "expression flags. E.g. IGNORECASE."
+            ),
+        },
+    }
+    output_variables = {
+        "result_output_var_name": {
+            "description": (
+                "Highest matched sub-pattern from input found on the fetched "
+                "URL. Note the actual name of variable depends on the input "
+                'variable "result_output_var_name" or is assigned a default of '
+                '"match."'
+            )
+        }
+    }
+
+    def prepare_curl_cmd(self) -> list[str]:
+        """Assemble curl command and return it."""
+        curl_cmd = super().prepare_curl_cmd()
+        self.add_curl_common_opts(curl_cmd)
+        curl_cmd.append(self.env["url"])
+        return curl_cmd
+
+    def prepare_re_flags(self) -> int:
+        """Create flag variable for re.compile"""
+        flag_accumulator = 0
+        for flag in self.env.get("re_flags", {}):
+            if flag in re.__dict__:
+                flag_accumulator += re.__dict__[flag]
+        return flag_accumulator
+
+    def re_search(self, content) -> tuple[str, dict[str, str]] | None:
+        """Search for re_pattern in content"""
+
+        re_pattern = re.compile(self.env["re_pattern"], flags=self.prepare_re_flags())
+        matches = re_pattern.findall(content)
+
+        if not matches:
+            raise ProcessorError(f"{NO_MATCH_MESSAGE}: {self.env['url']}")
+
+        # return the list of matches
+        return (matches)
+
+    def main(self) -> None:
+        output_var_name = self.env["result_output_var_name"]
+
+        # Prepare curl command
+        curl_cmd = self.prepare_curl_cmd()
+
+        # Execute curl command and search in content
+        content = self.download_with_curl(curl_cmd)
+        matchlist = self.re_search(content)
+
+        # Iterate through the list of matches to find the highest
+        highversion = "0"
+        for versionitem in matchlist:
+            if Version(versionitem) > Version(highversion):
+                highversion = versionitem
+
+        self.env[output_var_name] = highversion
+
+
+if __name__ == "__main__":
+    PROCESSOR = URLHighestVersionTextSearcher()
+    PROCESSOR.execute_shell()
